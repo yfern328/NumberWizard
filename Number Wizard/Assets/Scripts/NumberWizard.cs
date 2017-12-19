45639aa69c99f102de1732f0564da4a3eda7a3ea
yfern328 <yfernan2@u.rochester.edu>
2017-12-19 16:56:05 -0500

    understanding scope and implementing basic game logic

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -4,27 +4,35 @@ using System.Collections;
 public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+	
 	void Start () {
+		
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me!");
-		
-		int max = 1000;
-		int min = 1;
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
 		
-		print ("Is the number higher or lower than 500?");
-		print ("Up = higher, down = lower, return = equal"); 
+		print ("Is the number higher or lower than " + guess);
+		print ("Up = higher, down = lower, return = equal");
+		
+		max += 1; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			print ("Up arrow pressed");
+			min = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess);
 		}
 		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			print ("Down arrow pressed");
+			max = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess);
 		}
 		else if (Input.GetKeyDown(KeyCode.Return)) {
 			print ("I Won!");

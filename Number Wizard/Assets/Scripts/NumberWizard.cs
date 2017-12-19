13b6e67dee465b25738869770006daa76d9c63ae
yfern328 <yfernan2@u.rochester.edu>
2017-12-19 16:28:09 -0500

    basic conditional statements and getting user input

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -13,10 +13,21 @@ public class NumberWizard : MonoBehaviour {
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
+		
+		print ("Is the number higher or lower than 500?");
+		print ("Up = higher, down = lower, return = equal"); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			print ("Up arrow pressed");
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			print ("Down arrow pressed");
+		}
+		else if (Input.GetKeyDown(KeyCode.Return)) {
+			print ("I Won!");
+		}
 	}
 }
